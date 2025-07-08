2d69a57332c6b37f6291f033cf77713cea3a3777
Dylan Palmboom <nofxboy1234@gmail.com>
2025-07-08 20:47:22 +0200

    Test CRUD queries using drizzle

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { serve, SQL } from "bun";
 import index from "./index.html";
 import { drizzle } from "drizzle-orm/bun-sql";
+import { eq } from "drizzle-orm";
+import { usersTable } from "./db/schema";
 
 const client = new SQL(process.env.DATABASE_URL!);
 const db = drizzle({ client });
@@ -43,3 +45,36 @@ const server = serve({
 });
 
 console.log(`🚀 Server running at ${server.url}`);
+
+async function main() {
+  const user: typeof usersTable.$inferInsert = {
+    name: "John",
+    age: 30,
+    email: "john@example.com",
+  };
+
+  await db.insert(usersTable).values(user);
+  console.log("New user created!");
+
+  const users = await db.select().from(usersTable);
+  console.log("Getting all users from the database: ", users);
+
+  await db
+    .update(usersTable)
+    .set({
+      age: 31,
+    })
+    .where(eq(usersTable.email, user.email));
+  console.log("User info updated!");
+
+  const users2 = await db.select().from(usersTable);
+  console.log("Getting all users from the database: ", users2);
+
+  await db.delete(usersTable).where(eq(usersTable.email, user.email));
+  console.log("User deleted!");
+
+  const users3 = await db.select().from(usersTable);
+  console.log("Getting all users from the database: ", users3);
+}
+
+main();
