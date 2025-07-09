e4c203347bdfef3bd3f34066116c50bc5026e68c
Dylan Palmboom <nofxboy1234@gmail.com>
2025-07-09 15:26:07 +0200

    Add one-to-one relation to users table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,16 @@
+import { relations } from "drizzle-orm";
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+  invitedBy: integer("invited_by"),
 });
+
+export const usersRelations = relations(usersTable, ({ one }) => ({
+  invitee: one(usersTable, {
+    fields: [usersTable.invitedBy],
+    references: [usersTable.id],
+  }),
+}));
