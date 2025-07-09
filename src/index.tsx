6c015a9c3d6a2069bac43ee5af5989838d168e12
Dylan Palmboom <nofxboy1234@gmail.com>
2025-07-09 14:24:09 +0200

    Move drizzle query code to a module

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,5 @@
-import { serve, SQL } from "bun";
+import { serve } from "bun";
 import index from "./index.html";
-import { drizzle } from "drizzle-orm/bun-sql";
-import { eq } from "drizzle-orm";
-import { usersTable } from "./db/schema";
-
-const client = new SQL(process.env.DATABASE_URL!);
-const db = drizzle({ client });
 
 const server = serve({
   routes: {
@@ -45,35 +39,3 @@ const server = serve({
 });
 
 console.log(`🚀 Server running at ${server.url}`);
-
-async function main() {
-  // await db.delete(usersTable);
-
-  const user: typeof usersTable.$inferInsert = {
-    name: "John",
-    email: "john@example.com",
-  };
-
-  await db.insert(usersTable).values(user);
-  console.log("New user created!");
-
-  const users = await db.select().from(usersTable);
-  console.log("Getting all users from the database: ", users);
-
-  await db
-    .update(usersTable)
-    .set({ name: "Johnny" })
-    .where(eq(usersTable.email, user.email));
-  console.log("User info updated!");
-
-  const users2 = await db.select().from(usersTable);
-  console.log("Getting all users from the database: ", users2);
-
-  await db.delete(usersTable).where(eq(usersTable.email, user.email));
-  console.log("User deleted!");
-
-  const users3 = await db.select().from(usersTable);
-  console.log("Getting all users from the database: ", users3);
-}
-
-// main();
